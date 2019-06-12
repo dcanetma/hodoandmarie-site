cd965c222b1ab2138411ec2f0467e84e3e675092
dcanetma <dcanetma@gmail.com>
2019-06-13 01:20:20 +0200

    Play the video

--- themes/hodo/source/assets/js/app.js
+++ themes/hodo/source/assets/js/app.js
@@ -4,10 +4,11 @@ $(function() {
 
 	// @todo: play behaviour on post pages
 	$('#post .btn-play').click(function(e){
-		$('#post img.header').fadeTo('slow', 0.1, function(){
-				$(this).hide(function(){
-					$('#post .embed-responsive').show();
-				});
+		$('#post .embed-responsive').slideDown('slow', function(){
+			// Play the video
+
+		});
+		$('#post img.header').slideUp('slow', function(){
 		});
 	});
 });
