bad7a73d4c1f64a635c578007ad8202998558001
dcanetma <dcanetma@gmail.com>
2019-06-12 23:08:26 +0200

    Set right gutters to all

--- themes/hodo/scripts/index.js
+++ themes/hodo/scripts/index.js
@@ -1,3 +1,5 @@
+// A custom helper to be able to pass a color variable inside the <p> tag
+// To make contrast to the background the text is placed
 hexo.extend.helper.register('myMarkdown', function(text, color) {
 	text = '<span style="color: ' + color + '! important">' + text + '</span>';
 	const markdown = hexo.extend.helper.get('markdown').bind(this)
