2b8bedfd9a60fb7077524c1292ce6528214c5f77
dcanetma <dcanetma@gmail.com>
2019-06-13 00:49:15 +0200

    Video play actiob

--- themes/hodo/source/assets/js/app.js
+++ themes/hodo/source/assets/js/app.js
@@ -5,8 +5,9 @@ $(function() {
 	// @todo: play behaviour on post pages
 	$('#post .btn-play').click(function(e){
 		$('#post img.header').fadeTo('slow', 0.1, function(){
-				$(this).hide(function(){});
-				$('#post .embed-responsive').show();
+				$(this).hide(function(){
+					$('#post .embed-responsive').show();
+				});
 		});
 	});
 });
